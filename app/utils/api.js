8bece1215da40630308fbd9c2a7818db9403d08c
olympia <owojcik7@gmail.com>
2019-08-10 19:59:00 -0400

    Refactor from axios to fetch

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,24 +1,25 @@
-import axios from "axios";
-
 const id = "YOUR_CLIENT_ID";
 const sec = "YOUR_SECRET_ID";
 const params = `?client_id=${id}&client_secret=${sec}`;
 
 async function getProfile(username) {
-  const profile = axios.get(
+  const response = await fetch(
     `https://api.github.com/users/${username}${params}`
   );
-  return profile.data;
+  //axios automatically did the json for us, but with fetch we have to turn it into json
+  return response.json();
 }
 
-function getRepos(username) {
-  return axios.get(
+async function getRepos(username) {
+  const response = await fetch(
     `https://api.github.com/users/${username}/repos${params}&per_page=100`
   );
+
+  return response.json();
 }
 
 function getStarCount(repos) {
-  return repos.data.reduce(
+  return repos.reduce(
     (count, { stargazers_count }) => count + stargazers_count,
     0
   );
@@ -63,6 +64,7 @@ export async function fetchPopularRepos(language) {
       ${language}&sort=stars&order=desc&type=Repositories`
   );
 
-  const repos = await axios.get(encodedURI).catch(handleError);
-  return repos.data.items;
+  const response = await fetch(encodedURI).catch(handleError);
+  const repos = await response.json();
+  return repos.items;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   //the first item in array is the polyfill we installed
   //babel will make sure our entire app has everything included in babel/polyfill which includes Promise api/primitive so inside of our app we can now use promises
   //babel-polyfill includes support for async await
-  entry: ["@babel/polyfill", "./app/index.js"],
+  entry: ["@babel/polyfill", "whatwg-fetch", "./app/index.js"],
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "index_bundle.js",
