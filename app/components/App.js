e594fe804418764efaa5416b5113c844f0d03320
olympia <owojcik7@gmail.com>
2019-08-10 16:40:26 -0400

    Refactor App.js and PlayerPreview.js

--- app/components/App.js
+++ app/components/App.js
@@ -1,34 +1,31 @@
-
-var React = require('react');
-var ReactRouter = require('react-router-dom');
-var Router = ReactRouter.BrowserRouter;
-var Route = ReactRouter.Route;
-var Switch = ReactRouter.Switch;
-var Nav = require('./Nav');
-var Home = require('./Home');
-var Battle = require('./Battle');
-var Popular = require('./Popular');
-var Results = require('./Results');
+const React = require("react");
+const ReactRouter = require("react-router-dom");
+const Router = ReactRouter.BrowserRouter;
+const Route = ReactRouter.Route;
+const Switch = ReactRouter.Switch;
+const Nav = require("./Nav");
+const Home = require("./Home");
+const Battle = require("./Battle");
+const Popular = require("./Popular");
+const Results = require("./Results");
 
 class App extends React.Component {
   render() {
     return (
       <Router>
-        <div className='container'>
+        <div className="container">
           <Nav />
 
           <Switch>
-            <Route exact path='/' component={Home} />
-            <Route exact path='/battle' component={Battle} />
-            <Route path='/battle/results' component={Results} />
-            <Route path='/popular' component={Popular} />
-            <Route render={function () {
-              return <p>Not Found</p>
-            }} />
+            <Route exact path="/" component={Home} />
+            <Route exact path="/battle" component={Battle} />
+            <Route path="/battle/results" component={Results} />
+            <Route path="/popular" component={Popular} />
+            <Route render={() => <p>Not Found</p>} />
           </Switch>
         </div>
       </Router>
-    )
+    );
   }
 }
 
--- app/components/PlayerPreview.js
+++ app/components/PlayerPreview.js
@@ -1,25 +1,21 @@
-var React = require('react');
-var PropTypes = require('prop-types');
+const React = require("react");
+const PropTypes = require("prop-types");
 
-function PlayerPreview (props) {
+function PlayerPreview({ avatar, username, children }) {
   return (
     <div>
-      <div className='column'>
-        <img
-          className='avatar'
-          src={props.avatar}
-          alt={'Avatar for ' + props.username}
-        />
-        <h2 className='username'>@{props.username}</h2>
+      <div className="column">
+        <img className="avatar" src={avatar} alt={"Avatar for " + username} />
+        <h2 className="username">@{username}</h2>
       </div>
-      {props.children}
+      {children}
     </div>
-  )
+  );
 }
 
 PlayerPreview.propTypes = {
   avatar: PropTypes.string.isRequired,
-  username: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired
 };
 
 module.exports = PlayerPreview;
